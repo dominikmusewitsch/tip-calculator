2628ec6338a958adee37c24ce097f52858e4f6fa
dominikmusewitsch <dmusewitsch@gmail.com>
2025-10-07 11:57:29 +0200

    refactured code

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,59 @@
-function main() {
+import readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+async function main() {
+  const rl = readline.createInterface({ input, output });
   console.log("--- Tip Calculator ---");
-  const checkAmount: number = 50;
-  const correctCheckAmount = checkAmount.toFixed(2);
-  console.log(`Check Amount: $${correctCheckAmount}`);
-
-  const tipPercentage: number = 15;
-  console.log(`Tip Percentage: ${tipPercentage}%`);
-  const tipAmount: number = checkAmount * (tipPercentage / 100);
-  const correctTipAmount = tipAmount.toFixed(2);
-  console.log(`Tip Amount: $${correctTipAmount}`);
-  const totalBill: number = checkAmount + tipAmount;
-  const correctTotalBill = totalBill.toFixed(2);
-  console.log(`Total Bill: $${correctTotalBill}`);
-
-  const isDevided: boolean = true;
-  const splitByAmountPeople: number = 2;
-  console.log(`Divided among people: ${isDevided === true ? "yes" : "no"}`);
-
-  if (isDevided) {
-    console.log(`Split between how many people: ${splitByAmountPeople}`);
-    const pricePerPerson: number = totalBill / splitByAmountPeople;
-    const correctPricePerPerson = pricePerPerson.toFixed(2);
-    console.log(`Each person pays: $${correctPricePerPerson}`);
+  const amountStart = await rl.question("How high is the check?");
+  const amount: number = Number(amountStart.replace(",", "."));
+  if (Number.isNaN(amount) || amount <= 0) {
+    console.log("Invalid amount!");
+    rl.close();
+    return;
   }
+
+  const tipString = await rl.question("What percentage of tip will you give?");
+  const tipPercentage: number = Number(tipString.replace(",", "."));
+  if (Number.isNaN(tipPercentage) || amount < 0) {
+    console.log("Invalid tip percentage!");
+    rl.close();
+    return;
+  }
+
+  const splitString = (
+    await rl.question("Should the bill be split among multiple people? (y/n)")
+  ).toLowerCase();
+  const isDivided = ["y", "n"].includes(splitString);
+  let splitByAmountPeople = 1;
+
+  if (isDivided) {
+    const peopleStr = await rl.question(
+      "How many people will split the bill?: "
+    );
+    splitByAmountPeople = parseInt((peopleStr ?? "").trim(), 10);
+    if (!Number.isInteger(splitByAmountPeople) || splitByAmountPeople < 2) {
+      console.log("Invalid number of people!");
+      rl.close();
+      return;
+    }
+  }
+
+  const tipAmount: number = amount * (tipPercentage / 100);
+
+  const totalBill: number = amount + tipAmount;
+
+  console.log(`\nCheck Amount: $${amount}`);
+  console.log(`Tip Percentage: ${tipPercentage.toFixed(2)}%`);
+  console.log(`Tip Amount: $${tipAmount}`);
+  console.log(`Total Bill: $${totalBill}`);
+  console.log(`Divide among people:${isDivided ? "y" : "n"}`);
+
+  if (isDivided) {
+    const eachPersonPays = totalBill / splitByAmountPeople;
+    console.log(`Split between how many people: ${splitByAmountPeople}`);
+    console.log(`Each person pays: ${eachPersonPays.toFixed(2)}`);
+  }
+
   console.log("-----------------------------");
 }
 main();
